b9f2194a2d5539f0ef66751ebf004ceaa40f0c94
MatheusVinicio <144963193+MatheusVinicio@users.noreply.github.com>
2023-11-27 20:08:17 -0300

    Update adicionar-registro.js
    Criação da constante usuário logado.

--- codigo-fonte/area-logada/adicionar-registro/adicionar-registro.js
+++ codigo-fonte/area-logada/adicionar-registro/adicionar-registro.js
@@ -1,6 +1,8 @@
 const dadosAtuais = JSON.parse(localStorage.getItem("registroFormData")) || [];
 
 function salvarRegistro() {
+  const dadosUsuario = JSON.parse(localStorage.getItem("usuarioLogado")) || {};
+
   const registroForm = {
     nome: document.getElementById("nome-do-curso").value,
     cargaHoraria: document.getElementById("carga-horaria").value,
@@ -10,6 +12,7 @@ function salvarRegistro() {
     anexoCampo: document.getElementById("anexo-campo").value,
     setor: document.getElementById("setor").value,
     tipo: document.getElementById("tipo").value,
+    usuario: dadosUsuario.email,
   };
 
   dadosAtuais.push(registroForm);
