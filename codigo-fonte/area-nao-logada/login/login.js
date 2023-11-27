4a9dee1457619c6ddcd6462aa64b216c80b63066
MatheusVinicio <144963193+MatheusVinicio@users.noreply.github.com>
2023-11-27 20:04:46 -0300

    Update login.js
    Criação da função Limpar usuário, necessário para limpar os dados do usuário ao sair da aplicação.

--- codigo-fonte/area-nao-logada/login/login.js
+++ codigo-fonte/area-nao-logada/login/login.js
@@ -3,33 +3,40 @@ function createUsers() {
 
   const arrayUsers = [
     {
-      user: "matheusGestor",
+      user: "Qualifique Gestor",
       password: "55",
-      email: "matheus@gestor.com",
-      isTraining: true,
+      email: "qualifique@gestor.com",
+      isManager: true,
     },
     {
-      user: "matheusColaborador",
+      user: "Qualifique Colaborador",
       password: "55",
-      email: "matheus@colaborador.com",
-      isTraining: false,
+      email: "qualifique@colaborador.com",
+      isManager: false,
     },
   ];
 
   localStorage.setItem("users", JSON.stringify(arrayUsers));
 }
 
+function limpaUsuarioLogado() {
+  localStorage.setItem("usuarioLogado", {});
+}
+
 function autenticar(email, password) {
-  const arrayUsers = JSON.parse(localStorage.getItem("users"));
-  console.log("email", email);
-  console.log("password", password);
+  const arrayUsers = JSON.parse(localStorage.getItem("users")) || [];
   arrayUsers.forEach((_user) => {
     if (email === _user.email && password === _user.password) {
-      if (_user.isTraining) {
+      const usuarioLogado = { email, isManager: false, user: _user.user };
+
+      if (_user.isManager) {
+        usuarioLogado.isManager = _user.isManager;
+        localStorage.setItem("usuarioLogado", JSON.stringify(usuarioLogado));
         window.location.href = "../../area-logada/home-gestor/home-gestor.html";
         return;
       }
 
+      localStorage.setItem("usuarioLogado", JSON.stringify(usuarioLogado));
       window.location.href =
         "../../area-logada/home-colaborador/home-colaborador.html";
       return;
